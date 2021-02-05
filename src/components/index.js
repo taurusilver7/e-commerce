9460df0b850c273dbaf1ef099ec8008d8729153d
taurusilver7 <prasad20697@gmail.com>
2021-02-05 15:21:25 +0530

    easy export file added

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Home} from "./Home/Home";
+export {default as Footer} from './Footer/Footer';
+export {default as Header} from './Header/Header';
+export {default as Cart} from './Cart/Cart';
+export {default as Checkout} from './Checkout/Checkout';
