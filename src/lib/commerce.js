2e045e265e6f7c1b4ac3295e119bd2020116aaa5
taurusilver7 <prasad20697@gmail.com>
2021-02-05 15:18:55 +0530

    commerce instance completed

---
+++ src/lib/commerce.js
@@ -0,0 +1,7 @@
+import Commerce from "@chec/commerce.js";
+
+// a new instance of the commerce (store) is created. & give the public key of the store (chec account)
+export const commerce = new Commerce(
+  "pk_test_22725e96dcf157b89e48ebe2d29a011ed54c1d8bbe187",
+  true
+);
